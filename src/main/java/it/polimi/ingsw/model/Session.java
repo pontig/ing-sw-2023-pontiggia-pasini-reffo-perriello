86a62386117a45f40dd9bba3cfa8d5df23c00d7a
EliaPontiggia <pontiggiaelia@gmail.com>
2023-03-13 17:05:01 +0100

    packages setup

---
+++ src/main/java/it/polimi/ingsw/model/Session.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class Session {
+    public Session() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not Implemented yet");
+    }
+}
