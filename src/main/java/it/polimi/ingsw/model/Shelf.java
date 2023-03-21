ed564891dc064a98eb89b145eddc4635b6b271cd
Tommaso Reffo <tommaso.reffo@mail.polimi.it>
2023-03-21 14:09:51 +0100

    Implemented Shelf Class

--- src/main/java/it/polimi/ingsw/model/Shelf.java
+++ src/main/java/it/polimi/ingsw/model/Shelf.java
@@ -1,8 +1,9 @@
 package it.polimi.ingsw.model;
 
 import jdk.jshell.spi.ExecutionControl;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
+import java.util.*;
 
 public class Shelf {
     /* ============ SHELF SCHEMA: ============
@@ -14,25 +15,38 @@ public class Shelf {
       [0][5] [1][5] [2][5] [3][5] [4][5] ]
     ======================================= */
     private Item[][] items;
+    private List<Integer> insertableColumns;
+    private List<items> orderedItems;
+    private int columnChosen;
 
-    public Shelf() throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Class not Implemented yet");
+    public Shelf(items) {
+        this.items = new int[5][6];
+
+        for(int i = 0; i < 6; i++) {
+            for(int j = 0; j < 5; j++) {
+                items[i][j] = null;
+            }
+        }
+        for(int k = 0; k < 6; k++) {
+            this.insertableColumns = new ArrayList<>();            //inizialmente posso inserire in tutte le colonne
+            this.insertableColumns.add(k);
+        }
+        this.orderedItems = null;
+        this.columnChosen = -1;
     }
     public void printShelf() throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Method not Implemented yet");
+        throw new ExecutionControl.NotImplementedException("Method not Implemented yet"),
     }
     public void insertItem(Item item, int column) throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Method not Implemented yet");
     }
-    public int[] getInsertableColumns(int numItems) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Method not Implemented yet");
-    }
+    public int[] setInsertableColumns(int numItems) { this.numItems = numItems; }
 
-    public void insertItem(Item item, int column) throws ExecutionControl.NotImplementedException {
+    public List<items> getInsertableColumns() {
+        return insertableColumns;
+    }
+    public int getMaxFreeSpace() throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Method not Implemented yet");
     }
-    public int[] getInsertableColumns(int numItems) throws ExecutionControl.NotImplementedException {
-
-        throw new ExecutionControl.NotImplementedException("Method not Implemented yet");
-    }
+    public void setOrderItem(int posList) { this.posList = posList; }
 }
