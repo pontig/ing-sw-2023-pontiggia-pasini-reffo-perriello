977e36861b7415180d831e7bee4aa6a716d8ea52
Maurizio <maurizio.perriello@mail.polimi.it>
2023-03-22 12:02:27 +0100

    Implemented CommonGoal: FourAnglesGoal

--- src/main/java/it/polimi/ingsw/model/commongoal/FourAnglesGoal.java
+++ src/main/java/it/polimi/ingsw/model/commongoal/FourAnglesGoal.java
@@ -5,9 +5,11 @@ import it.polimi.ingsw.model.Shelf;
 public class FourAnglesGoal extends CommonGoalAbstract {
 
     //4 tiles of the same type in the four corners of the shelf
-
+    public FourAnglesGoal(int numberPlayers) {
+        super(numberPlayers);
+    }
     public boolean specificGoal(Shelf shelf) throws IllegalArgumentException {
 
-        return (shelf.item[0][0].equals(shelf.item[4][0]) && shelf.item[0][0].equals(shelf.item[4][5]) && shelf.item[0][0].equals(shelf.item[0][5]));
+        return ((shelf.item[0][0].equals(shelf.item[4][0])) && (shelf.item[0][0].equals(shelf.item[4][5])) && (shelf.item[0][0].equals(shelf.item[0][5])));
     }
 }
