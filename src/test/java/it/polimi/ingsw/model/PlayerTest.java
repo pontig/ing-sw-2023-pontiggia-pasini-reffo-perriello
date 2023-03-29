34d0160eb13e72bb27053019d70427630a699f73
EliaPontiggia <pontiggiaelia@gmail.com>
2023-03-29 11:57:56 +0200

    Begin of PlayerTest.java

---
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getNickname() {
+    }
+
+    @Test
+    void getPersonalGoal() {
+    }
+
+    @Test
+    void getShelf() {
+    }
+
+    @Test
+    void getFirstCommonScore() {
+    }
+
+    @Test
+    void setFirstCommonScore() {
+    }
+
+    @Test
+    void getSecondCommonScore() {
+    }
+
+    @Test
+    void setSecondCommonScore() {
+    }
+
+    @Test
+    void getEndGameToken() {
+    }
+
+    @Test
+    void setEndGameToken() {
+    }
+
+    @Test
+    void computeFinalScore() {
+    }
+}
