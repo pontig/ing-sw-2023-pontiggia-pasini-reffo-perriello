ac3cc8c4223c79cd42d012af2fe693f0e0eee639
Tommaso Reffo <tommaso.reffo@mail.polimi.it>
2023-03-30 20:22:30 +0200

    I set ShelfTest Class

---
+++ src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ShelfTest {
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void getItems() {
+    }
+
+    @Test
+    void setItems() {
+    }
+
+    @Test
+    void insertItems() {
+    }
+
+    @Test
+    void testInsertItems() {
+    }
+
+    @Test
+    void setInsertableColumns() {
+    }
+
+    @Test
+    void getInsertableColumns() {
+    }
+
+    @Test
+    void getMaxFreeSpace() {
+    }
+
+    @Test
+    void setItem() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void getColumns() {
+    }
+
+    @Test
+    void getRows() {
+    }
+
+    @Test
+    void adjacent() {
+    }
+}
