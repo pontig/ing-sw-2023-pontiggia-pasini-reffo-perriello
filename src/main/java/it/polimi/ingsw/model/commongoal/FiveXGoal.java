61c832f07ded86dca18439c84fbd365aef42a14c
EliaPontiggia <pontiggiaelia@gmail.com>
2023-04-03 18:11:37 +0200

    FiveXGoal and test

--- src/main/java/it/polimi/ingsw/model/commongoal/FiveXGoal.java
+++ src/main/java/it/polimi/ingsw/model/commongoal/FiveXGoal.java
@@ -1,56 +1,36 @@
 package it.polimi.ingsw.model.commongoal;
 
 
+import it.polimi.ingsw.model.Item;
+import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.enums.Type;
 
- import it.polimi.ingsw.model.Item;
- import it.polimi.ingsw.model.Shelf;
- import it.polimi.ingsw.model.enums.Type;
-
- import java.util.ArrayList;
- import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FiveXGoal extends CommonGoalAbstract {
 
-    List<Type> types;
-
-
     public FiveXGoal(int numberPlayers) {
         super(numberPlayers);
-        types = new ArrayList<>();
-        types.add(Type.BOOK);
-        types.add(Type.CAT);
-        types.add(Type.FRAME);
-        types.add(Type.GAME);
-        types.add(Type.PLANTS);
-        types.add(Type.TROPHY);
-
     }
 
-    /*
-        0 1 2 3 4
-      0 x x x x x
-      1 x x x x x
-      2 x b x b x
-      3 x x b x b
-      4 x b x b b
-      5 x x x x x
-   */
     public boolean specificGoal(Shelf shelf) throws IllegalArgumentException {
-
-        Item[][] playerShelf = shelf.getItems();
-        for(Type item: types) {
-            for (int r = 0; r < 6; r++) {
-                for (int c = 0; c < 5; c++) {
-                    if (item.equals(playerShelf[c][r].getType()) && item.equals(playerShelf[c+2][r].getType())
-                            && item.equals(playerShelf[c+2][r-2].getType()) && item.equals(playerShelf[c][r-2].getType())
-                            && item.equals(playerShelf[c + 1][r + 1].getType())) {
-                        return true;
-                    }
-                }
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 5; j++) {
+                if (shelf.getItem(i, j) != null
+                        && shelf.getItem(i + 1, j + 1) != null
+                        && shelf.getItem(i + 1, j - 1) != null
+                        && shelf.getItem(i - 1, j + 1) != null
+                        && shelf.getItem(i - 1, j - 1) != null
+                        && shelf.getItem(i, j).getType() == shelf.getItem(i + 1, j + 1).getType()
+                        && shelf.getItem(i, j).getType() == shelf.getItem(i + 1, j - 1).getType()
+                        && shelf.getItem(i, j).getType() == shelf.getItem(i - 1, j + 1).getType()
+                        && shelf.getItem(i, j).getType() == shelf.getItem(i - 1, j - 1).getType()
+                ) return true;
             }
         }
-         return false;
+        return false;
     }
 }
 
