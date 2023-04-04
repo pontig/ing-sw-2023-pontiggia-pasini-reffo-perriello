93a8332036d46df8d92542c208582b3ed3afb906
Tommaso Pasini <tommaso1.pasini@mail.polimi.it>
2023-04-04 18:48:47 +0200

    Inverted row and columns in Square2x2Goal and tested with the changement

--- src/main/java/it/polimi/ingsw/model/commongoal/Square2x2Goal.java
+++ src/main/java/it/polimi/ingsw/model/commongoal/Square2x2Goal.java
@@ -21,13 +21,14 @@ public class Square2x2Goal extends CommonGoalAbstract{
     }
 
     /*
-         0 1 2 3 4
-       0 x x x a a
-       1 c x x t t
-       2 c c x t t
-       3 c t t b b
-       4 a t t b b
-       5 a a b b b
+      0 1 2 3 4 5   r
+    0 x x x x x x
+    1 x x x x x x
+    2 x x x x x x
+    3 x x x x x x
+    4 x x x x x x
+
+    c
     */
 
     public boolean specificGoal(Shelf shelf){
@@ -40,16 +41,16 @@ public class Square2x2Goal extends CommonGoalAbstract{
             playerShelf = shelfClone.getItems();
             for(int r = 5; r > 0; r--) {
                 for(int c = 0; c < 4; c++) {
-                    if(playerShelf[r][c] == null || playerShelf[r][c+1] == null || playerShelf[r-1][c] == null || playerShelf[r-1][c+1] == null)
+                    if(playerShelf[c][r] == null || playerShelf[c+1][r] == null || playerShelf[c][r-1] == null || playerShelf[c+1][r-1] == null)
                         continue;
 
-                    if(item.equals(playerShelf[r][c].getType()) && item.equals(playerShelf[r][c+1].getType())
-                            && item.equals(playerShelf[r-1][c].getType()) && item.equals(playerShelf[r-1][c+1].getType())) {
+                    if(item.equals(playerShelf[c][r].getType()) && item.equals(playerShelf[c+1][r].getType())
+                            && item.equals(playerShelf[c][r-1].getType()) && item.equals(playerShelf[c+1][r-1].getType())) {
                         counter++;
-                        playerShelf[r][c] = null;
-                        playerShelf[r][c+1] = null;
-                        playerShelf[r-1][c] = null;
-                        playerShelf[r-1][c+1] = null;
+                        playerShelf[c][r] = null;
+                        playerShelf[c+1][r] = null;
+                        playerShelf[c][r-1] = null;
+                        playerShelf[c+1][r-1] = null;
                         if(counter == 2)
                             return true;
                     }
--- src/test/java/it/polimi/ingsw/model/commongoal/Square2x2GoalTest.java
+++ src/test/java/it/polimi/ingsw/model/commongoal/Square2x2GoalTest.java
@@ -22,8 +22,8 @@ class Square2x2GoalTest {
 
 
         //full
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 5; j++)
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 6; j++)
                 playerShelf.setItem(i, j, new Item(BOOK, 1));
         }
         assertEquals(true, twoPlayers.specificGoal(playerShelf));
@@ -32,148 +32,142 @@ class Square2x2GoalTest {
 
 
         /*all condition of null
-         0 1 2 3 4
-       0 a a x a x
-       1 a x a a a
-       2 x x x x x
-       3 a a x a a
-       4 x x a x a
-       5 x x a x x
+          0 1 2 3 4 5
+        0 a a x a x x
+        1 a x x a x x
+        2 x a x x a a
+        3 a a x a x x
+        4 x a x a a x
          */
         playerShelf.clear();
         playerShelf.setItem(0, 0, new Item(BOOK,0));
         playerShelf.setItem(0, 1, new Item(BOOK,1));
         playerShelf.setItem(0, 3, new Item(BOOK,2));
         playerShelf.setItem(1, 0, new Item(BOOK,0));
-        playerShelf.setItem(1, 2, new Item(BOOK,1));
-        playerShelf.setItem(1, 3, new Item(BOOK,2));
-        playerShelf.setItem(1, 4, new Item(BOOK,0));
-        playerShelf.setItem(3, 0, new Item(BOOK,1));
-        playerShelf.setItem(3, 1, new Item(BOOK,2));
-        playerShelf.setItem(3, 3, new Item(BOOK,0));
-        playerShelf.setItem(3, 4, new Item(BOOK,1));
-        playerShelf.setItem(4, 2, new Item(BOOK,2));
-        playerShelf.setItem(4, 4, new Item(BOOK,0));
-        playerShelf.setItem(5, 2, new Item(BOOK,1));
-        playerShelf.setItem(5, 3, new Item(BOOK,2));
+        playerShelf.setItem(1, 3, new Item(BOOK,1));
+        playerShelf.setItem(2, 1, new Item(BOOK,2));
+        playerShelf.setItem(2, 4, new Item(BOOK,0));
+        playerShelf.setItem(2, 5, new Item(BOOK,1));
+        playerShelf.setItem(3, 0, new Item(BOOK,2));
+        playerShelf.setItem(3, 1, new Item(BOOK,0));
+        playerShelf.setItem(3, 3, new Item(BOOK,1));
+        playerShelf.setItem(4, 1, new Item(BOOK,2));
+        playerShelf.setItem(4, 3, new Item(BOOK,0));
+        playerShelf.setItem(4, 4, new Item(BOOK,1));
         assertEquals(false, twoPlayers.specificGoal(playerShelf));
         assertEquals(false, threePlayers.specificGoal(playerShelf));
         assertEquals(false, fourPlayers.specificGoal(playerShelf));
 
 
         /*boundaries limit
-         0 1 2 3 4
-       0 a a a a a
-       1 x x x x a
-       2 x x x x a
-       3 x x x x a
-       4 x x x x a
-       5 x x x x a
+          0 1 2 3 4 5
+        0 a x x x x x
+        1 a x x x x x
+        2 a x x x x x
+        3 a x x x x x
+        4 a a a a a a
          */
         playerShelf.clear();
         playerShelf.setItem(0, 0, new Item(BOOK,0));
-        playerShelf.setItem(0, 1, new Item(BOOK,1));
-        playerShelf.setItem(0, 2, new Item(BOOK,2));
-        playerShelf.setItem(0, 3, new Item(BOOK,0));
-        playerShelf.setItem(0, 4, new Item(BOOK,1));
-        playerShelf.setItem(1, 4, new Item(BOOK,1));
-        playerShelf.setItem(2, 4, new Item(BOOK,1));
-        playerShelf.setItem(3, 4, new Item(BOOK,1));
+        playerShelf.setItem(1, 0, new Item(BOOK,1));
+        playerShelf.setItem(2, 0, new Item(BOOK,2));
+        playerShelf.setItem(3, 0, new Item(BOOK,0));
+        playerShelf.setItem(4, 0, new Item(BOOK,1));
+        playerShelf.setItem(4, 1, new Item(BOOK,1));
+        playerShelf.setItem(4, 2, new Item(BOOK,1));
+        playerShelf.setItem(4, 3, new Item(BOOK,1));
         playerShelf.setItem(4, 4, new Item(BOOK,1));
-        playerShelf.setItem(5, 4, new Item(BOOK,1));
+        playerShelf.setItem(4, 5, new Item(BOOK,1));
         assertEquals(false, twoPlayers.specificGoal(playerShelf));
         assertEquals(false, threePlayers.specificGoal(playerShelf));
         assertEquals(false, fourPlayers.specificGoal(playerShelf));
 
 
         /*two 2x2 square on boundaries angles
-         0 1 2 3 4
-       0 x x x a a
-       1 x x x a a
-       2 x x x x x
-       3 x x x x x
-       4 x x x a a
-       5 x x x a a
+          0 1 2 3 4 5
+        0 x x x x x x
+        1 x x x x x x
+        2 x x x x x x
+        3 a a x x a a
+        4 a a x x a a
          */
         playerShelf.clear();
-        playerShelf.setItem(0, 3, new Item(BOOK,0));
-        playerShelf.setItem(0, 4, new Item(BOOK,1));
-        playerShelf.setItem(1, 3, new Item(BOOK,2));
-        playerShelf.setItem(1, 4, new Item(BOOK,0));
-        playerShelf.setItem(4, 3, new Item(BOOK,1));
-        playerShelf.setItem(4, 4, new Item(BOOK,2));
-        playerShelf.setItem(5, 3, new Item(BOOK,0));
-        playerShelf.setItem(5, 4, new Item(BOOK,1));
+        playerShelf.setItem(3, 0, new Item(BOOK,0));
+        playerShelf.setItem(3, 1, new Item(BOOK,1));
+        playerShelf.setItem(3, 4, new Item(BOOK,2));
+        playerShelf.setItem(3, 5, new Item(BOOK,0));
+        playerShelf.setItem(4, 0, new Item(BOOK,1));
+        playerShelf.setItem(4, 1, new Item(BOOK,2));
+        playerShelf.setItem(4, 4, new Item(BOOK,0));
+        playerShelf.setItem(4, 5, new Item(BOOK,1));
         assertEquals(true, twoPlayers.specificGoal(playerShelf));
         assertEquals(true, threePlayers.specificGoal(playerShelf));
         assertEquals(true, fourPlayers.specificGoal(playerShelf));
 
 
         /*overlapping 2x2 square
-         0 1 2 3 4
-       0 x x x x x
-       1 x x b b b
-       2 x x b b b
-       3 a a b b b
-       4 a a a x x
-       5 a a a x x
+          0 1 2 3 4 5
+        0 x x x a a a
+        1 x x x a a a
+        2 x b b b a a
+        3 x b b b x x
+        4 x b b b x x
          */
         playerShelf.clear();
-        playerShelf.setItem(1, 2, new Item(BOOK,0));
-        playerShelf.setItem(1, 3, new Item(BOOK,1));
-        playerShelf.setItem(1, 4, new Item(BOOK,2));
-        playerShelf.setItem(2, 2, new Item(BOOK,0));
-        playerShelf.setItem(2, 3, new Item(BOOK,1));
-        playerShelf.setItem(2, 4, new Item(BOOK,2));
-        playerShelf.setItem(3, 0, new Item(CAT,0));
-        playerShelf.setItem(3, 1, new Item(CAT,1));
-        playerShelf.setItem(3, 2, new Item(BOOK,0));
-        playerShelf.setItem(3, 3, new Item(BOOK,1));
-        playerShelf.setItem(3, 4, new Item(BOOK,2));
-        playerShelf.setItem(4, 0, new Item(CAT,2));
-        playerShelf.setItem(4, 1, new Item(CAT,0));
-        playerShelf.setItem(4, 2, new Item(CAT,1));
-        playerShelf.setItem(5, 0, new Item(CAT,2));
-        playerShelf.setItem(5, 1, new Item(CAT,0));
-        playerShelf.setItem(5, 2, new Item(CAT,1));
+        playerShelf.setItem(0, 3, new Item(BOOK,0));
+        playerShelf.setItem(0, 4, new Item(BOOK,1));
+        playerShelf.setItem(0, 5, new Item(BOOK,2));
+        playerShelf.setItem(1, 3, new Item(BOOK,0));
+        playerShelf.setItem(1, 4, new Item(BOOK,1));
+        playerShelf.setItem(1, 5, new Item(BOOK,2));
+        playerShelf.setItem(2, 1, new Item(CAT,0));
+        playerShelf.setItem(2, 2, new Item(CAT,1));
+        playerShelf.setItem(2, 3, new Item(CAT,0));
+        playerShelf.setItem(2, 4, new Item(BOOK,1));
+        playerShelf.setItem(2, 5, new Item(BOOK,2));
+        playerShelf.setItem(3, 1, new Item(CAT,2));
+        playerShelf.setItem(3, 2, new Item(CAT,0));
+        playerShelf.setItem(3, 3, new Item(CAT,1));
+        playerShelf.setItem(4, 1, new Item(CAT,2));
+        playerShelf.setItem(4, 2, new Item(CAT,0));
+        playerShelf.setItem(4, 3, new Item(CAT,1));
         assertEquals(false, twoPlayers.specificGoal(playerShelf));
         assertEquals(false, threePlayers.specificGoal(playerShelf));
         assertEquals(false, fourPlayers.specificGoal(playerShelf));
 
 
         /*generic shelf two 2x2
-         0 1 2 3 4
-       0 x x x a a
-       1 c x x t t
-       2 c c x t t
-       3 c t t b b
-       4 a t t b b
-       5 a a b b b
+          0 1 2 3 4 5
+        0 x c c c a a
+        1 x x c t t a
+        2 x x x t t b
+        3 a t t b b b
+        4 a t t b b b
          */
-        playerShelf.setItem(0, 3, new Item(BOOK, 0));
-        playerShelf.setItem(0, 4, new Item(BOOK, 1));
-        playerShelf.setItem(1, 0, new Item(CAT, 0));
-        playerShelf.setItem(1, 3, new Item(TROPHY, 0));
-        playerShelf.setItem(1, 4, new Item(TROPHY, 1));
-        playerShelf.setItem(2, 0, new Item(CAT, 1));
-        playerShelf.setItem(2, 1, new Item(CAT, 2));
-        playerShelf.setItem(2, 3, new Item(TROPHY, 2));
-        playerShelf.setItem(2, 4, new Item(TROPHY, 0));
-        playerShelf.setItem(3, 0, new Item(CAT, 0));
-        playerShelf.setItem(3, 1, new Item(TROPHY, 1));
+        playerShelf.setItem(0, 1, new Item(CAT, 0));
+        playerShelf.setItem(0, 2, new Item(CAT, 1));
+        playerShelf.setItem(0, 3, new Item(CAT, 0));
+        playerShelf.setItem(0, 4, new Item(BOOK, 0));
+        playerShelf.setItem(0, 5, new Item(BOOK, 1));
+        playerShelf.setItem(1, 2, new Item(CAT, 1));
+        playerShelf.setItem(1, 3, new Item(TROPHY, 2));
+        playerShelf.setItem(1, 4, new Item(TROPHY, 2));
+        playerShelf.setItem(1, 5, new Item(BOOK, 0));
+        playerShelf.setItem(2, 3, new Item(TROPHY, 0));
+        playerShelf.setItem(2, 4, new Item(TROPHY, 1));
+        playerShelf.setItem(2, 5, new Item(PLANTS, 2));
+        playerShelf.setItem(3, 0, new Item(BOOK, 0));
+        playerShelf.setItem(3, 1, new Item(TROPHY, 0));
         playerShelf.setItem(3, 2, new Item(TROPHY, 2));
-        playerShelf.setItem(3, 3, new Item(PLANTS, 0));
-        playerShelf.setItem(3, 4, new Item(PLANTS, 0));
-        playerShelf.setItem(4, 0, new Item(CAT, 2));
-        playerShelf.setItem(4, 1, new Item(TROPHY, 2));
-        playerShelf.setItem(4, 2, new Item(TROPHY, 2));
+        playerShelf.setItem(3, 3, new Item(PLANTS, 2));
+        playerShelf.setItem(3, 4, new Item(PLANTS, 2));
+        playerShelf.setItem(3, 5, new Item(PLANTS, 0));
+        playerShelf.setItem(4, 0, new Item(BOOK, 0));
+        playerShelf.setItem(4, 1, new Item(TROPHY, 0));
+        playerShelf.setItem(4, 2, new Item(TROPHY, 1));
         playerShelf.setItem(4, 3, new Item(PLANTS, 0));
         playerShelf.setItem(4, 4, new Item(PLANTS, 0));
-        playerShelf.setItem(5, 0, new Item(BOOK, 0));
-        playerShelf.setItem(5, 1, new Item(BOOK, 1));
-        playerShelf.setItem(5, 2, new Item(PLANTS, 0));
-        playerShelf.setItem(5, 3, new Item(PLANTS, 0));
-        playerShelf.setItem(5, 4, new Item(PLANTS, 0));
+        playerShelf.setItem(4, 5, new Item(PLANTS, 0));
         assertEquals(true, twoPlayers.specificGoal(playerShelf));
         assertEquals(true, threePlayers.specificGoal(playerShelf));
         assertEquals(true, fourPlayers.specificGoal(playerShelf));
