0801c26bbf59bfda61de62e265bb87b752513786
Tommaso Reffo <tommaso.reffo@mail.polimi.it>
2023-04-04 12:14:38 +0200

    Tested EightSameTypeGoal and modified EightSameTypeGoal

--- src/main/java/it/polimi/ingsw/model/commongoal/EightSameTypeGoal.java
+++ src/main/java/it/polimi/ingsw/model/commongoal/EightSameTypeGoal.java
@@ -1,33 +1,59 @@
 package it.polimi.ingsw.model.commongoal;
 
-import it.polimi.ingsw.model.Board;
 import it.polimi.ingsw.model.Item;
 import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.enums.Type;
 
-    public class EightSameTypeGoal extends CommonGoalAbstract {
-        //Eight tiles of the same type. There are no restrictions on the locations of these tiles
-        int count = 0;
-        boolean check = false;
+import java.util.ArrayList;
+import java.util.List;
 
-        public EightSameTypeGoal(int numberPlayers) {
-            super(numberPlayers);
-        }
+public class EightSameTypeGoal extends CommonGoalAbstract {
+    //Eight tiles of the same type. There are no restrictions on the locations of these tiles
+    List<Type> types;
 
-        public boolean specificGoal(Shelf shelf) throws IllegalArgumentException {
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 5; j++) {
-                    Item itemtest = shelf.getItem(i,j);
-                    for (int k = 0; k < 6; k++) {
-                        for (int h = 0; h < 5; h++) {
-                            if (shelf.getItem(k,h).equals(itemtest) && k != i && h != j) {
-                                count++;
-                            }
-                        }
-                        boolean check = (count == 8) ? true : false;
+    public EightSameTypeGoal(int numberPlayers) {
+        super(numberPlayers);
+        types = new ArrayList<>();
+        types.add(Type.BOOK);
+        types.add(Type.CAT);
+        types.add(Type.FRAME);
+        types.add(Type.GAME);
+        types.add(Type.PLANTS);
+        types.add(Type.TROPHY);
+    }
+      /*
+         0 1 2 3 4
+       0 x x x a x
+       1 a x x x a
+       2 x a x x x
+       3 x x x a a
+       4 a x x x x
+       5 a x x x x
+    */
+
+    public boolean specificGoal(Shelf shelf) throws IllegalArgumentException {
+        int counter = 0;
+        Shelf shelfClone;
+        Item[][] playerShelf;
+
+        for (Type item : types) {
+            shelfClone = shelf.clone();
+            playerShelf = shelfClone.getItems();
+            for (int c = 0; c < 5; c++) {
+                for (int r = 0; r < 6; r++) {
+                    if (playerShelf[c][r] == null)
+                        continue;
+                    if (item.equals(playerShelf[c][r].getType())) {
+                        counter++;
+                        playerShelf[c][r] = null;
+                        if (counter >= 8)
+                            return true;
                     }
                 }
             }
-            return check;
+            counter = 0;
         }
+           return false;
     }
+}
 
--- src/test/java/it/polimi/ingsw/model/commongoal/EightSameTypeGoalTest.java
+++ src/test/java/it/polimi/ingsw/model/commongoal/EightSameTypeGoalTest.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.model.commongoal;
 
+import it.polimi.ingsw.model.Item;
 import it.polimi.ingsw.model.Shelf;
+import it.polimi.ingsw.model.enums.Type;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +12,49 @@ class EightSameTypeGoalTest {
     CommonGoalAbstract threePlayers = new EightSameTypeGoal(3);
     CommonGoalAbstract fourPlayers = new EightSameTypeGoal(4);
 
-    Shelf Playershelf = new Shelf();
+    Shelf PlayerShelf = new Shelf();
     @Test
     void specificGoal() {
 
+        //empty
+        assertFalse(twoPlayers.specificGoal(PlayerShelf));
+        assertFalse( threePlayers.specificGoal(PlayerShelf));
+        assertFalse(fourPlayers.specificGoal(PlayerShelf));
 
+
+
+
+        //case true
+        Shelf PlayerShelf = new Shelf();
+        PlayerShelf.setItem(0, 0, new Item(Type.BOOK,0));
+        PlayerShelf.setItem(0, 1, new Item(Type.BOOK,1));
+        PlayerShelf.setItem(0, 2, new Item(Type.BOOK,2));
+        PlayerShelf.setItem(0, 3, new Item(Type.BOOK,0));
+        PlayerShelf.setItem(0, 4, new Item(Type.BOOK,1));
+        PlayerShelf.setItem(1, 4, new Item(Type.BOOK,1));
+        PlayerShelf.setItem(2, 4, new Item(Type.BOOK,1));
+        PlayerShelf.setItem(3, 4, new Item(Type.BOOK,1));
+
+       assertTrue(twoPlayers.specificGoal(PlayerShelf));
+       assertTrue( threePlayers.specificGoal(PlayerShelf));
+       assertTrue(fourPlayers.specificGoal(PlayerShelf));
+
+
+       //case false
+        Shelf PlayerShelf2 = new Shelf();
+        PlayerShelf2.setItem(0, 0, new Item(Type.BOOK,0));
+        PlayerShelf2.setItem(0, 3, new Item(Type.BOOK,1));
+        PlayerShelf2.setItem(0, 2, new Item(Type.CAT,2));
+        PlayerShelf2.setItem(1, 3, new Item(Type.BOOK,0));
+        PlayerShelf2.setItem(0, 4, new Item(Type.BOOK,1));
+        PlayerShelf2.setItem(1, 4, new Item(Type.BOOK,1));
+        PlayerShelf2.setItem(2, 4, new Item(Type.FRAME,1));
+        PlayerShelf2.setItem(2, 1, new Item(Type.BOOK,1));
+        PlayerShelf2.setItem(4, 4, new Item(Type.BOOK,1));
+
+        assertFalse(twoPlayers.specificGoal(PlayerShelf2));
+        assertFalse( threePlayers.specificGoal(PlayerShelf2));
+        assertFalse(fourPlayers.specificGoal(PlayerShelf2));
 
     }
 
