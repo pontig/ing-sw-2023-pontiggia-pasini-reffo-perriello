bdd62e7c727a0c7f80d3a211a216da9754c4826b
Maurizio <maurizio.perriello@mail.polimi.it>
2023-05-02 14:52:31 +0200

    select new version pt.2

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -98,6 +98,7 @@ public class Board {
                 } else {
                     sameLineOrColumn = false;
                 }
+                pendingCells.addAll(list);
             }
         }
         if (sameLineOrColumn) {
