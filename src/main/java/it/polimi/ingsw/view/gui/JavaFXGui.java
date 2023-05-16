947595ccd5d73f6a74a7f4079d33a94289f0753b
EliaPontiggia <pontiggiaelia@gmail.com>
2023-05-16 17:47:59 +0200

    finally i can show at least one window

---
+++ src/main/java/it/polimi/ingsw/view/gui/JavaFXGui.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.view.gui;
+
+import it.polimi.ingsw.view.GUI;
+import it.polimi.ingsw.view.gui.scene.NicknameSceneController;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Main JavaFX class which starts the main stage and scene.
+ * we have to choose between swing or javafx
+ * It extends {@link Application}
+ */
+public class JavaFXGui extends Application {
+
+    /**
+     * Starts the JavaFXGui instance
+     *
+     * @param stage the stage
+     */
+    @Override
+    public void start(Stage stage) {
+
+
+        GUI view = new GUI();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/NicknameScene.fxml"));
+        Parent rootLayout = null;
+        try {
+            rootLayout = loader.load();
+        } catch (IOException e) {
+            System.exit(1);
+        }
+
+
+        NicknameSceneController controller = loader.getController();
+
+        // Show the scene containing the root layout.
+        Scene scene = new Scene(rootLayout);
+        SceneController.setActiveScene(scene);
+        SceneController.setActiveController(controller);
+        SceneController.setMainStage(stage);
+        stage.setScene(scene);
+        stage.setWidth(900);
+        stage.setHeight(600);
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
+        stage.setResizable(true);
+        stage.setTitle("La mia libreria");
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/mother_nature.png")));
+        stage.show();
+    }
+
+    /**
+     * Stops the ongoing instance
+     */
+    @Override
+    public void stop() {
+        Platform.exit();
+        System.exit(0);
+    }
+}
