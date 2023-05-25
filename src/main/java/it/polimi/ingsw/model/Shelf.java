fff3bd9e4979a2e2446bb4ba5ae1f6cfbc1d8370
Tommaso Reffo <tommaso.reffo@mail.polimi.it>
2023-05-25 16:58:39 +0200

    Some adjustment

--- src/main/java/it/polimi/ingsw/model/Shelf.java
+++ src/main/java/it/polimi/ingsw/model/Shelf.java
@@ -125,8 +125,8 @@ public class Shelf {
                    count = 0;
                    break;
                }
-               count = 0;
             }
+            count = 0;
         }
     }
 
