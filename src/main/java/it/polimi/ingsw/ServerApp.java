1996df271998220a0bc96c3104b67b77e419bc72
Tommaso Pasini <tommaso1.pasini@mail.polimi.it>
2023-06-01 17:23:13 +0200

    Revert "tests with rmi"
    This reverts commit ec761d308486eaa9713e3037041ebecdd9ec46c8.

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -5,14 +5,12 @@ import it.polimi.ingsw.network.server.Server;
 import it.polimi.ingsw.network.server.ServerImpl;
 
 import java.io.IOException;
-import java.net.*;
-import java.nio.channels.SocketChannel;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,7 +19,6 @@ public class ServerApp extends UnicastRemoteObject implements ServerAbst {
     private static ServerApp instance = null;
     private static Server s = null;
     private final ExecutorService executorService = Executors.newCachedThreadPool();
-    private static String serverIP;
 
     protected ServerApp() throws RemoteException {
     }
@@ -37,61 +34,12 @@ public class ServerApp extends UnicastRemoteObject implements ServerAbst {
         return s;
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-
-        // iterate over the network interfaces known to java
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-        OUTER:
-        for (NetworkInterface interface_ : Collections.list(interfaces)) {
-            // we shouldn't care about loopback addresses
-            if (interface_.isLoopback())
-                continue;
-
-            // if you don't expect the interface to be up you can skip this
-            // though it would question the usability of the rest of the code
-            if (!interface_.isUp())
-                continue;
-
-            // iterate over the addresses associated with the interface
-            Enumeration<InetAddress> addresses = interface_.getInetAddresses();
-            for (InetAddress address : Collections.list(addresses)) {
-                // look only for ipv4 addresses
-                if (address instanceof Inet6Address)
-                    continue;
-
-                // use a timeout big enough for your needs
-                if (!address.isReachable(3000))
-                    continue;
-
-                // java 7's try-with-resources statement, so that
-                // we close the socket immediately after use
-                //try (SocketChannel socket = SocketChannel.open()) {
-                //    // again, use a big enough timeout
-                //    socket.socket().setSoTimeout(3000);
-                //
-                //    // bind the socket to your local interface
-                //    socket.bind(new InetSocketAddress(address, 8080));
-                //
-                //    // try to connect to *somewhere*
-                //    socket.connect(new InetSocketAddress("google.com", 80));
-                //} catch (IOException ex) {
-                //    ex.printStackTrace();
-                //    continue;
-                //}
-
-                System.out.format("ni: %s, ia: %s\n", interface_, address);
-                serverIP = address.toString().substring(1);
-
-                // stops at the first *working* solution
-                break OUTER;
-            }
-        }
-
+    public static void main(String[] args) throws RemoteException {
         Scanner terminal = new Scanner(System.in);
         Integer port = 0;
         final int portRMI;
         final int portSocket;
-        System.out.print("\nInsert Server RMI port number - 4 digit only: ");
+        /*System.out.print("\nInsert Server RMI port number - 4 digit only: ");
         do {
             try {
                 port = Integer.parseInt(terminal.next());
@@ -114,8 +62,11 @@ public class ServerApp extends UnicastRemoteObject implements ServerAbst {
             if (port.toString().length() != 4)
                 System.out.print("Enter a 4 digit number only: ");
         } while (port.toString().length() != 4);
-        portSocket = port;
-        //RMI
+        portSocket = port;*/
+        // TODO: debug only
+        portRMI = 1111;
+        portSocket = 2222;
+//RMI
         Thread rmiThread = new Thread(() -> {
             try {
                 startRMI(portRMI);
@@ -133,10 +84,10 @@ public class ServerApp extends UnicastRemoteObject implements ServerAbst {
             }
         });
         socketThread.start();
-        //Connection RMI e Socket
+//Connection RMI e Socket
         try {
             rmiThread.join();
-            System.out.println("\nServer working on address " + serverIP);
+            System.out.println("\nServer working");
             socketThread.join();
         } catch (InterruptedException e) {
             System.err.println("No connection protocol available. Exiting...");
@@ -147,7 +98,8 @@ public class ServerApp extends UnicastRemoteObject implements ServerAbst {
     private static void startRMI(int port) throws RemoteException {
         System.out.println("RMI started");
         ServerApp instanceRMI = getInstance();
-        System.setProperty("java.rmi.server.hostname", serverIP);
+        // TODO: remember quello che ha detto il prof
+        //System.setProperty("java.rmi.server.hostname", "192.168.104.128");
         Registry registry = LocateRegistry.createRegistry(port);
         registry.rebind("server", instanceRMI);
     }
