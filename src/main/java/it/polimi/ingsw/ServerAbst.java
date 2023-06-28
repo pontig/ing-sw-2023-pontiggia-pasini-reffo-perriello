8f7cce6e6d7867f8bcee2eb3a9d9032ccb1a2f62
Tommaso Reffo <tommaso.reffo@mail.polimi.it>
2023-06-28 15:16:12 +0200

    I just started Javadocs of the ClientApp Class

--- src/main/java/it/polimi/ingsw/ServerAbst.java
+++ src/main/java/it/polimi/ingsw/ServerAbst.java
@@ -5,6 +5,10 @@ import it.polimi.ingsw.network.server.Server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * The ServerAbst interface defines the contract for a remote server.
+ * It provides methods for connecting to the server.
+ */
 public interface ServerAbst extends Remote {
     Server connect() throws RemoteException;
 }
